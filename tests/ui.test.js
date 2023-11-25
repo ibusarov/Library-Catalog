79bd57ac5f5a978880b8e5b46b85dbad20ca232c
Igor Busarov <igobusarov@yahoo.com>
2023-11-26 00:19:21 +0200

    Add tests 2 and 3

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,11 +1,32 @@
 const { expect, test } = require("@playwright/test")
+const pageUrl = "http://localhost:3000"
 
 test('Verify "All Books" link is visible', async ({page}) => {
-    await page.goto('http://localhost:3000')
+    await page.goto(pageUrl)
     await page.waitForSelector('nav.navbar')
 
     const allBooksLink = await page.$('a[href="/catalog"]')
     const isLinkVisible = await allBooksLink.isVisible()
 
     expect(isLinkVisible).toBe(true)
+})
+
+test('Verify Login button is visible', async ({page}) => {
+    await page.goto(pageUrl)
+    await page.waitForSelector('nav.navbar')
+
+    const loginButton = await page.$('a[href="/login"]')
+    const isButtonVisible = await loginButton.isVisible()
+
+    expect(isButtonVisible).toBe(true)
+})
+
+test('Verify Regisgter button is visible', async ({page}) => {
+    await page.goto(pageUrl)
+    await page.waitForSelector('nav.navbar')
+
+    const registerButton = await page.$('a[href="/register"]')
+    const isButtonVisible = await registerButton.isVisible()
+
+    expect(isButtonVisible).toBe(true)
 })
